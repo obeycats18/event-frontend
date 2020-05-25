1311138cbdf0f51365045111b3983151e3033080
obeycats18 <54187413+obeycats18@users.noreply.github.com>
2020-05-25 23:56:35 +0300

    Add files via upload

--- pages/finish.js
+++ pages/finish.js
@@ -26,4 +26,56 @@ const finishOrder = () => {
     })
 }
 
+const renderModal = () => {
+
+
+    const url = new URL('http:/localhost:8080/order')
+    url.searchParams.set('lastName', localStorage.getItem('lastName'))
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 200) {
+                const order = data.order
+                console.log(order)
+                let doc = new DOMParser()
+                const modalBody = document.querySelector('.modal-body')
+                if (modalBody.children.length > 0) {
+                    modalBody.innerHTML = ''
+                }
+
+                let orderString = `
+                   <ul class="order-detail-list">
+                       <li> <b>Фамилия</b> ${order.last_name} </li>
+                       <li> <b>Имя</b> ${order.first_name} </li>
+                       <li> <b>Телефон</b> ${order.telephone} </li>
+                       <li> <b>Дата</b> ${order.date} </li>
+                       <li> <b>Тип мероприятия</b> ${order.typeEvent} </li>
+                       <li> 
+                        <b>Блюда</b>
+                        <ul class="order-detail-list-menu">
+                             ${order.editedDishes.map( dish => `<li>${dish.name}</li>`).join(' ')}
+                        </ul>
+                       </li>
+                       <li> 
+                        <b>Доп.услуги</b>
+                        <ul class="order-detail-list-services">
+                             ${order.services.map( service => `<li>${service.name}</li>`).join(' ')}
+                        </ul>
+                       </li>
+                   </ul>
+                `
+
+
+                const docMenu = doc.parseFromString(orderString, 'text/html');
+                modalBody.appendChild(docMenu.body.firstChild)
+            }
+        })
+}
+renderModal()
 finishOrder()
